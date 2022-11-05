f2df19f21eb7e267f1a84e759534241ff0cc8356
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-11-05 13:40:10 +0545

    login form created successfully

--- Day3/src/App.jsx
+++ Day3/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./components/Login";
 function App() {
   return (
     <>
-      <main>
+      <main className=' bg-sky-400 h-screen w-screen flex justify-center items-center'>
         <Login />
       </main>
     </>
--- Day3/src/components/Login.jsx
+++ Day3/src/components/Login.jsx
@@ -1,7 +1,76 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const Login = () => {
-  return <></>;
+  const getdata = (data) => {
+    console.log(data);
+  };
+  const schema = yup.object().shape({
+    username: yup
+      .string()
+      .email("mail did not match format - test@example.com")
+      .required("Username is required"),
+    password: yup
+      .string()
+      .required("Password is required")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+        "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+      ),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+  return (
+    <form
+      onSubmit={handleSubmit(getdata)}
+      className=' bg-sky-700 flex flex-col p-4 w-1/5 rounded-md'>
+      <h1 className=' text-white font-semibold text-3xl'>Login</h1>
+      <label className='text-white mt-3' htmlFor=''>
+        Username:
+      </label>
+      <input
+        type='text'
+        className='border-2 border-sky-500 rounded-md p-1'
+        {...register("username", { required: true })}
+      />
+      {errors.username && (
+        <p className=' text-red-400 bg-slate-700 mt-3 p-2 rounded-md'>
+          {errors.username.message}
+        </p>
+      )}
+      <label className='text-white mt-3' htmlFor=''>
+        Password:
+      </label>
+      <input
+        className='border-2 border-sky-500 rounded-md p-1'
+        type='password'
+        {...register("password", { required: true })}
+      />
+      {errors.password && (
+        <p className=' text-red-400 bg-slate-700 mt-3 p-2 rounded-md'>
+          {errors.password.message}
+        </p>
+      )}
+
+      <button
+        className='mt-3 bg-sky-300 text-white font-medium text-xl p-2 rounded-lg disabled:bg-sky-200 disabled:text-slate-400 '
+        type='submit'>
+        Login
+      </button>
+      <p className=' text-slate-300 mt-2'>Not Registered yet?</p>
+      <p className=' text-white text-slate-300 underline'>
+        <a href='#'>Sign up</a>
+      </p>
+    </form>
+  );
 };
 
 export default Login;
