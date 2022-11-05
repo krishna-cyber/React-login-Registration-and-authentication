c7359f8c9430abf76ed47227d5306e9b5faa9a18
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-11-05 18:09:40 +0545

    minor changes

--- Day3/src/components/Login.jsx
+++ Day3/src/components/Login.jsx
@@ -4,9 +4,6 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 const Login = () => {
-  const getdata = (data) => {
-    console.log(data);
-  };
   const schema = yup.object().shape({
     username: yup
       .string()
@@ -23,53 +20,65 @@ const Login = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitted, isSubmitSuccessful },
   } = useForm({
     resolver: yupResolver(schema),
     mode: "onChange",
   });
+  const getdata = async (data) => {
+    console.log(data);
+    setSubmit(isSubmitSuccessful);
+    console.log(isSubmitSuccessful);
+  };
+  const [submit, setSubmit] = React.useState(false);
   return (
-    <form
-      onSubmit={handleSubmit(getdata)}
-      className=' bg-sky-700 flex flex-col p-4 w-1/5 rounded-md'>
-      <h1 className=' text-white font-semibold text-3xl'>Login</h1>
-      <label className='text-white mt-3' htmlFor=''>
-        Username:
-      </label>
-      <input
-        type='text'
-        className='border-2 border-sky-500 rounded-md p-1'
-        {...register("username", { required: true })}
-      />
-      {errors.username && (
-        <p className=' text-red-400 bg-slate-700 mt-3 p-2 rounded-md'>
-          {errors.username.message}
-        </p>
-      )}
-      <label className='text-white mt-3' htmlFor=''>
-        Password:
-      </label>
-      <input
-        className='border-2 border-sky-500 rounded-md p-1'
-        type='password'
-        {...register("password", { required: true })}
-      />
-      {errors.password && (
-        <p className=' text-red-400 bg-slate-700 mt-3 p-2 rounded-md'>
-          {errors.password.message}
-        </p>
-      )}
+    <>
+      {submit ? (
+        <div>Form Submitted Successfully</div>
+      ) : (
+        <form
+          onSubmit={handleSubmit(getdata)}
+          className=' bg-sky-700 flex flex-col p-4 w-1/5 rounded-md'>
+          <h1 className=' text-white font-semibold text-3xl'>Login</h1>
+          <label className='text-white mt-3' htmlFor=''>
+            Username:
+          </label>
+          <input
+            type='text'
+            className='border-2 border-sky-500 rounded-md p-1'
+            {...register("username", { required: true })}
+          />
+          {errors.username && (
+            <p className=' text-red-400 bg-slate-700 mt-3 p-2 rounded-md'>
+              {errors.username.message}
+            </p>
+          )}
+          <label className='text-white mt-3' htmlFor=''>
+            Password:
+          </label>
+          <input
+            className='border-2 border-sky-500 rounded-md p-1'
+            type='password'
+            {...register("password", { required: true })}
+          />
+          {errors.password && (
+            <p className=' text-red-400 bg-slate-700 mt-3 p-2 rounded-md'>
+              {errors.password.message}
+            </p>
+          )}
 
-      <button
-        className='mt-3 bg-sky-300 text-white font-medium text-xl p-2 rounded-lg disabled:bg-sky-200 disabled:text-slate-400 '
-        type='submit'>
-        Login
-      </button>
-      <p className=' text-slate-300 mt-2'>Not Registered yet?</p>
-      <p className=' text-white text-slate-300 underline'>
-        <a href='#'>Sign up</a>
-      </p>
-    </form>
+          <button
+            className='mt-3 bg-sky-300 text-white font-medium text-xl p-2 rounded-lg disabled:bg-sky-200 disabled:text-slate-400 '
+            type='submit'>
+            Login
+          </button>
+          <p className=' text-slate-300 mt-2'>Not Registered yet?</p>
+          <p className=' text-white text-slate-300 underline'>
+            <a href='#'>Sign up</a>
+          </p>
+        </form>
+      )}
+    </>
   );
 };
 
