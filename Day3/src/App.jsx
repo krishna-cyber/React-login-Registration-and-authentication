b420b33ae0fbbf381a634fb43ee286f418997e80
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-11-05 13:20:13 +0545

    day3 started

---
+++ Day3/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <main>
+        <Login />
+      </main>
+    </>
+  );
+}
+
+export default App;
